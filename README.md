2c5cad51fa90a433d7eeceedc655af48a9220934
yeluorag <yeluorag@gmail.com>
2016-08-12 16:03:38 +0800

    'update'

--- README.md
+++
@@ -1,2 +0,0 @@
-# ACM-ICPC-Code-Library
-ACM Code
--- 数学/组合数/Combo.cpp
+++
@@ -1,9 +0,0 @@
-LL C[N][N];
-void Combo() {
-	mem(C, 0);
-	for(int i = 0; i < N; i ++) {
-		C[i][0] = 1;
-		for(int j = 1; j <= i; j ++)
-			C[i][j] = (C[i-1][j-1] + C[i-1][j]) % MOD;
-	}
-}
